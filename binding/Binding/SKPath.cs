65c9af79aadfe2ca5c9fa78fa470c051a4cfdec8
Jacquelyn Vinson <jacqvinson@gmail.com>
2022-04-26 17:29:22 -0700

    Improve the hot path of the path iterators (#3)
    Expose the raw pointers for the Next method so consumers can opt into a much more performant run.
    
    Co-authored-by: Matthew Leibowitz <mattleibow@live.com>

--- binding/Binding/SKPath.cs
+++ binding/Binding/SKPath.cs
@@ -545,8 +545,16 @@ namespace SkiaSharp
 					throw new ArgumentException ("Must be an array of four elements.", nameof (points));
 
 				fixed (SKPoint* p = points) {
-					return SkiaApi.sk_path_iter_next (Handle, p);
+					return Next(p);
 				}
+			}
+
+			public SKPathVerb Next (SKPoint* points)
+			{
+				if (points == null)
+					throw new ArgumentNullException (nameof (points));
+
+				return SkiaApi.sk_path_iter_next (Handle, points);
 			}
 
 			public float ConicWeight () =>
@@ -584,9 +592,18 @@ namespace SkiaSharp
 					throw new ArgumentNullException (nameof (points));
 				if (points.Length != 4)
 					throw new ArgumentException ("Must be an array of four elements.", nameof (points));
+
 				fixed (SKPoint* p = points) {
-					return SkiaApi.sk_path_rawiter_next (Handle, p);
+					return Next (p);
 				}
+			}
+
+			public SKPathVerb Next (SKPoint* points)
+			{
+				if (points == null)
+					throw new ArgumentNullException (nameof (points));
+
+				return SkiaApi.sk_path_rawiter_next (Handle, points);
 			}
 
 			public float ConicWeight () =>
